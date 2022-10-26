15e9825af2aef86918c6cb2deefeb1a21d9ac765
Can Ata Tekirdağlı <canata.tekirdagli@darussafaka.net>
2022-10-27 00:31:13 +0300

    Add project files.

---
+++ MvcYazGelProje/Controllers/UyeLoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcYazGelProje.Controllers
+{
+    public class UyeLoginController : Controller
+    {
+        // GET: UyeLogin
+        public ActionResult OgrenciGiris()
+        {
+            return View();
+        }
+        public ActionResult OgretmenGiris()
+        {
+            return View();
+        }
+        public ActionResult SifremiUnuttum()
+        {
+            return View();
+        }
+
+    }
+}
