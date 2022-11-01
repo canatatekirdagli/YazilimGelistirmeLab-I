0cc5e9270bbb37f1577390692c49c2c3e9ec542a
Can Ata Tekirdağlı <canata.tekirdagli@darussafaka.net>
2022-11-01 19:38:54 +0300

    .

--- MvcYazGelProje/Controllers/OgrenciPanelController.cs
+++ MvcYazGelProje/Controllers/OgrenciPanelController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,8 +28,33 @@ namespace MvcYazGelProje.Controllers
         }
         public ActionResult Stajİsleri()
         {
+            string[] files = Directory.GetFiles(Server.MapPath("~/App_Data/uploads"));
+            string[] fileNames = new string[files.Count()];
+            for (int i = 0; i < files.Count(); i++)
+            {
+                fileNames[i] = files[i].Substring(files[i].IndexOf("uploads"));
+            }
+            TempData["files"] = fileNames;
             return View();
         }
+        [HttpPost]
+        public ActionResult StajIsleri(IEnumerable<HttpPostedFileBase> files, string desc)
+        {
+            foreach (var file in files)
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = desc + "_" + Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/Uploads"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+            return RedirectToAction("StajIsleri");
+        }
+
+
+
+
         public ActionResult IMEIsleri()
         {
             return View();
