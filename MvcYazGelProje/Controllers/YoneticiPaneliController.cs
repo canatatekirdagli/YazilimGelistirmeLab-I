562c8320c0489307c161c9eff7da92f2781dc2ba
Can Ata Tekirdağlı <canata.tekirdagli@darussafaka.net>
2022-11-02 06:01:56 +0300

    .

--- MvcYazGelProje/Controllers/YoneticiPaneliController.cs
+++ MvcYazGelProje/Controllers/YoneticiPaneliController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
@@ -30,21 +32,34 @@ namespace MvcYazGelProje.Controllers
         public ActionResult KullaniciEkle(FormCollection form)
         {
             string randomPassword = Membership.GeneratePassword(10, 2);
-            string sifre = Sifrele.MD5Olustur(randomPassword);
-            uye uye = new uye();
-            uye.uyeAd = form["uyeAd"];
-            uye.uye_no = form["uye_no"];
-            uye.uyeSoyad = form["uyeSoyad"];
-            uye.uyeEposta = form["uyeEposta"];
-            uye.uye_gsm = form["uye_gsm"];
-            uye.uye_tc = form["uye_tc"];
-            uye.uye_bolumAd = form["uye_bolumAd"];
-            uye.uye_gorevi = "Öğrenci";
-            uye.IME_durumu = false;
-            uye.uye_sifre = sifre;
-            db.uye.Add(uye);
-            db.SaveChanges();
-            return View();
+            //SmtpClient client = new SmtpClient();
+            //client.Credentials = new NetworkCredential("canatatekirdagli30@gmail.com", "ŞİFRE");
+            //client.Port = 587;
+            //client.Host = "smtp.gmail.com";
+            //client.EnableSsl = true;
+            //MailMessage mail = new MailMessage();
+            //mail.To.Add("canata.coc@gmail.com");
+            //mail.From = new MailAddress("canatatekirdagli30@gmail.com","Şifre Gönderiminiz");
+            //mail.IsBodyHtml = true;
+            //mail.Subject = "Şifreniz";
+            //mail.Body += "Merhaba siteme hoş geldiniz :) <br/> Sisteme giriş yaparken kullanacağınız; <br/> Mail: " + /*form["uyeEposta"]+*/ "<br/> Şifre: "+randomPassword + "<br/> Sisteme girdikten sonra şifrenizi değiştirmeyi unutmayın!";
+           
+            //    client.Send(mail);
+                string sifre = Sifrele.MD5Olustur(randomPassword);
+                uye uye = new uye();
+                uye.uyeAd = form["uyeAd"];
+                uye.uye_no = form["uye_no"];
+                uye.uyeSoyad = form["uyeSoyad"];
+                uye.uyeEposta = form["uyeEposta"];
+                uye.uye_gsm = form["uye_gsm"];
+                uye.uye_tc = form["uye_tc"];
+                uye.uye_bolumAd = form["uye_bolumAd"];
+                uye.uye_gorevi = "Öğrenci";
+                uye.IME_durumu = false;
+                uye.uye_sifre = sifre;
+                db.uye.Add(uye);
+                db.SaveChanges();
+                return View();
         }
 
 
@@ -53,6 +68,21 @@ namespace MvcYazGelProje.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult KullaniciSil(FormCollection form)
+        {
+            var kullanici1 = db.dosya.Find(form["id"]);
+            db.dosya.Remove(kullanici1);
+            var kullanici2 = db.form.Find(form["id"]);
+            db.form.Remove(kullanici2);
+            var kullanici = db.uye.Find(form["id"]);
+            db.uye.Remove(kullanici);
+            db.SaveChanges();
+            return RedirectToAction("KullaniciIslemleri");
+        }
+
+
+
         public ActionResult RolTanimlama()
         {
             return View();
@@ -73,7 +103,7 @@ namespace MvcYazGelProje.Controllers
         public ActionResult StajGoruntulemeDetay(int id)
         {
             var bilgi = db.form.Find(id);
-            return View("Detay", bilgi);
+            return View("StajGoruntulemeDetay", bilgi);
         }
 
 
@@ -89,7 +119,17 @@ namespace MvcYazGelProje.Controllers
         public ActionResult StajDegerlendirmeDetay(int id)
         {
             var bilgi = db.form.Find(id);
-            return View("DegerlendirDetay", bilgi);
+            return View("StajDegerlendirmeDetay", bilgi);
+        }
+
+        public ActionResult Guncelle(form p)
+        {
+            var belge = db.form.Find(p.staj_id);
+            belge.stajNotu = p.stajNotu;
+            belge.sorumlu = p.sorumlu;
+            belge.basvuruDurumu = p.basvuruDurumu;
+            db.SaveChanges();
+            return RedirectToAction("StajDegerlendirme");
         }
 
 
@@ -129,6 +169,8 @@ namespace MvcYazGelProje.Controllers
         {
             return View();
         }
+
+
         public ActionResult YoneticiGuncelleme()
         {
             return View();
